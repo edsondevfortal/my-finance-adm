3caa6ba15f0b3b6c52c202597915b264d5ef997f
Edson Aguiar <edsondev.fortal@gmail.com>
2023-03-22 08:56:00 -0300

    :hammer: adjust import TransactionTable component

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import { Summary } from "../Summary";
-import { TransitionTable } from "../TransitionTable";
+import { TransitionTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {

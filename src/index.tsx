9a860e314b824dcc5bc9ff2ad40a64a26ca070ab
Edson Aguiar <edsondev.fortal@gmail.com>
2023-03-21 22:39:52 -0300

    :pincel: add mock data and get route

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createServer } from "miragejs";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return [
+        {
+          id: 1,
+          title: "mock",
+          value: 203.02,
+          category: "mock",
+          data: "00/00/00",
+        },
+        {
+          id: 2,
+          title: "mock",
+          value: 203.02,
+          category: "mock",
+          data: "00/00/00",
+        },
+        {
+          id: 3,
+          title: "mock",
+          value: 244.02,
+          category: "mock",
+          data: "00/00/00",
+        },
+        {
+          id: 4,
+          title: "mock",
+          value: 5.622,
+          category: "mock",
+          data: "00/00/00",
+        },
+      ];
+    });
+  },
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<App />);

9a42f56bd05133c30d789fc292000db3570318fb
Edson Aguiar <edsondev.fortal@gmail.com>
2023-03-21 22:25:41 -0300

    :hammer: add dashboard Summary and styles

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,34 @@
+import { Container } from "./styles";
+import income from "../../assets/income.png";
+import outcome from "../../assets/outcome.png";
+import total from "../../assets/total.png";
+
+export function Summary() {
+  return (
+    <>
+      <Container>
+        <div className="bg-income">
+          <header>
+            <p>Entradas</p>
+            <img src={income} alt="Entradas" />
+          </header>
+          <strong>1000,00</strong>
+        </div>
+        <div className="bg-outcome">
+          <header>
+            <p>Saídas</p>
+            <img src={outcome} alt="Saídas" />
+          </header>
+          <strong> - 500,00</strong>
+        </div>
+        <div className="bg-balance">
+          <header>
+            <p>Saldo</p>
+            <img src={total} alt="Total" />
+          </header>
+          <strong>500,00</strong>
+        </div>
+      </Container>
+    </>
+  );
+}
---
+++ src/components/Summary/styles.ts
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  margin-top: -4rem;
+  div {
+    padding: 1.5rem 2rem;
+    border-radius: 0.25rem;
+    box-shadow: 0 3px 3px -4px;
+
+    header {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      color: #146c8a;
+      font-weight: 500;
+      font-size: 1.1rem;
+      img {
+        width: 33px;
+        height: 33px;
+      }
+    }
+    strong {
+      display: block;
+      margin-top: 1rem;
+      font-size: 2rem;
+      font-weight: normal;
+      line-height: 3rem;
+      color: #424242;
+    }
+
+    &.bg-income {
+      background: #f3ffda;
+    }
+
+    &.bg-outcome {
+      background: #f6f6f6;
+      strong {
+        color: #b84c4c;
+      }
+    }
+
+    &.bg-balance {
+      background: #f3ffda;
+      
+    }
+  }
+`;

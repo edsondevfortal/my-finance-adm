fc709fbe63e46b5eb34c8688a16b1cb3acf9e120
Edson Aguiar <edsondev.fortal@gmail.com>
2023-03-21 22:35:31 -0300

    :pincel: add dashboard Components in App file

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Globalstyle } from "./styles/global";
+import { Header } from "./components/Header";
+import { Dashboard } from "./components/Dashboard";
+import { useState } from "react";
+import ReactModal from "react-modal";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
-function App() {
+// accessibility | informing that the modal is on the page
+ReactModal.setAppElement("#root");
+
+export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function transactionModal() {
+    if (!isNewTransactionModalOpen) {
+      setIsNewTransactionModalOpen(true);
+      return;
+    }
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header openNewTransactionModal={transactionModal} />
+      <Dashboard />
+      <Globalstyle />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={transactionModal}
+      />
+    </>
   );
 }
-
-export default App;

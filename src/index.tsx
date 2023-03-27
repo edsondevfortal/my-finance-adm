55e504e4c19fac7bcc892ada0e933bf09c65f4da
Edson Aguiar <edsondev.fortal@gmail.com>
2023-03-27 15:46:51 -0300

    add create post, table entity and get request from the mirageJS

--- src/index.tsx
+++ src/index.tsx
@@ -1,43 +1,51 @@
-import { createServer } from "miragejs";
-import React from "react";
+import { createServer, Model } from "miragejs";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
 createServer({
+  // mirageJS Model | create transaction as the table entity
+  models: {
+    transaction: Model,
+  },
+
+  // seeds(server) {
+  //   server.db.loadData({
+  //     transactions: [
+  //       {
+  //         id: 1,
+  //         title: "Mercado",
+  //         amount: 350,
+  //         category: "Alimentação",
+  //         type: "entrada",
+  //         date: new Date("2023-06-02 19:40:33"),
+  //       },
+  //       {
+  //         id: 2,
+  //         title: "Terapia",
+  //         amount: 120,
+  //         category: "Saúde",
+  //         type: "saida",
+  //         date: new Date("2023-06-01 21:00:33"),
+  //       },
+  //     ],
+  //   });
+  // },
+
   routes() {
     this.namespace = "api";
 
+    // create get request
     this.get("/transactions", () => {
-      return [
-        {
-          id: 1,
-          title: "mock",
-          value: 203.02,
-          category: "mock",
-          data: "00/00/00",
-        },
-        {
-          id: 2,
-          title: "mock",
-          value: 203.02,
-          category: "mock",
-          data: "00/00/00",
-        },
-        {
-          id: 3,
-          title: "mock",
-          value: 244.02,
-          category: "mock",
-          data: "00/00/00",
-        },
-        {
-          id: 4,
-          title: "mock",
-          value: 5.622,
-          category: "mock",
-          data: "00/00/00",
-        },
-      ];
+      return this.schema.all("transaction");
+    });
+
+    // create post request
+    this.post("/transactions", (schema, request) => {
+      // insert the data into the data variable and convert the request text type to object JS
+      const data = JSON.parse(request.requestBody);
+
+      // insert the data into the transaction entity
+      return schema.create("transaction", data);
     });
   },
 });
